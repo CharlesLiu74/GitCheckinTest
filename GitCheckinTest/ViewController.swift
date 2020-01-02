99879a274923cede094c4ba4bc7c69ea1774a4b0
Charles Liu <chiacheng74@icloud.com>
2020-01-02 12:00:41 +0800

    Initial Commit

---
+++ GitCheckinTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitCheckinTest
+//
+//  Created by Charles Liu on 2020/1/2.
+//  Copyright © 2020 Zinwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
